50ac161c26479e631b7572afd0444264b91d6ac7
indiana <gioargyr@gmail.com>
2017-01-26 12:24:53 +0200

    Constants for docker

--- eventDetection/src/utils/Constants.java
+++ eventDetection/src/utils/Constants.java
@@ -7,12 +7,12 @@ public class Constants {
     public static String outputFolderAmatrice = "/amattrice/";
     public static String outputFolderAbudhabi = "/sarabiaresult/";
     public static String outputFolderGeneral = "/result/";
-    public static String outputFolder = "//media/indiana/data/dockers/dockereventstore/tobemounted/";
-    public static String strabonHost = "localhost";
-    public static int strabonPort = 8192;
-//    public static String outputFolder = "/snap/";
-//    public static String strabonHost = "semagrow_bde";
-//    public static int strabonPort = 8080;
+//    public static String outputFolder = "//media/indiana/data/dockers/dockereventstore/tobemounted/";
+//    public static String strabonHost = "localhost";
+//    public static int strabonPort = 8192;
+    public static String outputFolder = "/snap/";
+    public static String strabonHost = "semagrow_bde";
+    public static int strabonPort = 8080;
 
     
 }
